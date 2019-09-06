30558e8cbe586521c8aa7a5e13b6ece1b0046fff
Nikola Barisic <barisic.nb@gmail.com>
2019-09-06 14:11:26 +0200

    swagger default json

--- Providers/AdonisSwaggerProvider.js
+++ Providers/AdonisSwaggerProvider.js
@@ -6,13 +6,13 @@ class AdonisSwaggerProvider extends ServiceProvider {
     register () {
         this.app.bind('Adonis/Commands/SwaggerJSON', () => require('../commands/SwaggerJSON'))
         this.app.bind('Adonis/Commands/SwaggerUI', () => require('../commands/SwaggerUI'))
-        this.app.bind('Adonis/Commands/SwaggerUI', () => require('../commands/SwaggerUI'))
+        this.app.bind('Adonis/Commands/SwaggerDefault', () => require('../commands/SwaggerDefault'))
     }
 
     boot () {
         ace.addCommand('Adonis/Commands/SwaggerJSON')
         ace.addCommand('Adonis/Commands/SwaggerUI')
-        ace.addCommand('Adonis/Commands/SwaggerUI')
+        ace.addCommand('Adonis/Commands/SwaggerDefault')
     }
 }
 
